5054fc654819d81db1e1de0d58781eae8222892d
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-05-30 13:29:27 +0530

    V-7

---
+++ HydraUB/plugins/alive.py
@@ -0,0 +1,41 @@
+"""
+This Telegram Userbot is provided "as is" without warranty of any kind, either express or implied. The author(s) and/or distributor(s) of this Userbot shall not be liable for any damages arising from the use of this Userbot.
+
+By using this Userbot, you agree to the terms and conditions set forth in this license. If you do not agree to these terms and conditions, do not use this Userbot.
+
+This Userbot may be used by anyone, provided that credit is given to Hyper Speed™.
+
+The author(s) and/or distributor(s) reserve the right to modify or discontinue this Userbot at any time without notice.
+
+All rights reserved to Hyper Speed™.
+"""
+import time 
+import random 
+import asyncio
+from config import HANDLER, OWNER_ID, HS
+from pyrogram import filters, __version__ as pyrover
+from HydraUB import get_readable_time, StartTime
+from HydraUB import Hydra as HS
+
+
+911 = "Lᴏᴀᴅɪɴɢ HʏᴅʀᴀUB"
+
+
+@HS.on_message(filters.command("alive",prefixes=HANDLER) & filters.user(OWNER_ID))
+async def alive(_, message):
+    name = (await HS.get_me()).first_name
+    await message.edit(911)
+    await asyncio.sleep(3)
+    await message.delete()
+    alive = await message.reply_animation(HS, caption="")
+    await alive.edit_caption(f"Hello Master **{name}**,\nYou Are Using HydraUserBot V-7 And Your Current Pyrogram Version Is {pyrover}!")
+
+
+@HS.on_message(filters.command("ping",prefixes=HANDLER) & filters.user(OWNER_ID))
+async def ping(_, message):
+     start_time = time.time()
+     end_time = time.time()
+     ping_time = round((end_time - start_time) * 1000, 3)
+     uptime = get_readable_time((time.time() - StartTime))
+     await message.edit(f"👾 **System Uptime & Ping**\n=> 🔔 **Ping**: {ping_time}\n=> ⬆️ **Uptime**: {uptime}")
+
---
+++ HydraUB/plugins/clone.py
@@ -0,0 +1,72 @@
+from Hydra import Hydra as HS
+from pyrogram import filters
+from HydraUB.DB.clone_db import store_profile, get_profile
+import config
+
+@HS.on_message(filters.command("cpfp",config.HANDLER) & filters.user(config.OWNER_ID))
+async def clone(_, message):
+    if not message.reply_to_message:
+         try:
+            clone_id = message.text.split(None,1)[1]
+         except:
+              return await message.edit("=> reply to the user either give a user id")
+    else:
+         clone_id = message.reply_to_message.from_user.id
+
+    user_id = message.from_user.id
+    
+    if (await get_profile(user_id)) == False:
+          return await message.edit_text("You didn't Saved Any Profile. Send ```.savepfp``` and try again.") 
+           
+          
+    await message.edit('Collecting Information from Client')
+
+    user = await HS.get_chat(clone_id)
+    bio = user.bio if user.bio else None
+    first_name = user.first_name
+    photo_id = user.photo.big_file_id if user.photo else None
+
+    try:
+       profile = await HS.download_media(photo_id)
+       await HS.set_profile_photo(photo=profile)
+    except:
+        pass
+   
+    await HS.update_profile(first_name=first_name, bio=bio)
+    return await message.edit("✅ Successfully Implemented!")
+    
+    
+    
+@HS.on_message(filters.command("savepfp", config.HANDLER) & filters.user(config.OWNER_ID))
+async def save_pfp(_, message):
+      user_id = message.from_user.id
+      await message.edit('Saving your information into DB')      
+      user = await HS.get_chat(user_id)
+      bio = user.bio if user.bio else None
+      first_name = user.first_name 
+      async for file in HS.get_chat_photos(user_id, limit=1):
+             photo_id = file.file_id if user.photo else None
+      await store_profile(user_id=user_id, profile=photo_id, first_name=first_name, bio=bio)
+      return await message.edit("Successfully Saved!")
+          
+@HS.on_message(filters.command("rnpfp", config.HANDLER) & filters.user(config.OWNER_ID))
+async def return_profile(_, message):
+     user_id = message.from_user.id
+     if (await get_profile(user_id)) == False:
+         return await message.edit("Use ```.savepfp``` save your information and try again.")      
+     user = await get_profile(user_id)
+     bio = user.get("bio")
+     first_name = user.get("first_name")
+     photo_id = user.get("profile")
+     try:
+        profile = await HS.download_media(photo_id)
+        await HS.set_profile_photo(photo=profile)
+     except:
+         pass
+
+     await HS.update_profile(first_name=first_name, bio=bio)
+     return await message.edit("Successfully Reseted Info!")
+
+
+
+
